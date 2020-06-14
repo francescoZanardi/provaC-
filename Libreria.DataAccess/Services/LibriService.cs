4f3bc1e3236e0b56e7120b9e4a26675ef5d6bc4f
Francesco Zanardi <zanardifrancesco98@gmail.com>
2020-06-14 18:30:10 +0200

    Creazione del primo services

---
+++ Libreria.DataAccess/Services/LibriService.cs
@@ -0,0 +1,33 @@
+﻿using Libreria.DataAccess.DbMidels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.DataAccess.Services
+{
+    public class LibriService
+    {
+        private readonly LibreriaContext _libreriaContext;
+        public LibriService(LibreriaContext libreriaContext)
+        {
+            _libreriaContext = libreriaContext;
+        }
+        public async Task <List<Libro>>GetLibro()
+        {
+            try
+            {
+                var result = await _libreriaContext.Libros
+                    .Include(x => x.Libreria)
+                    .Include(x => x.LibroAutores)
+                    .ThenInclude(x => x.Autore).ToListAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
